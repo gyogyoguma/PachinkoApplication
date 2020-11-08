046001d15debd983319382e8af999430906451b9
gyogyoguma <gyogyoguma@gmail.com>
2020-11-08 16:38:29 +0900

    Push!!!!!

--- app/src/main/java/com/example/pachinkoapplication/MainActivity.java
+++ app/src/main/java/com/example/pachinkoapplication/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-//gfhfgｊｈｊ
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
