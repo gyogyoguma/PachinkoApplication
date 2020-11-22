c048bfd5f67cc6a61f7bb53b4db52a602c74e645
ten3sei <tensho6686@gmail.com>
2020-11-22 16:20:33 +0900

    せやな

--- app/src/main/java/com/example/pachinkoapplication/MainActivity.java
+++ app/src/main/java/com/example/pachinkoapplication/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//douya?
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
